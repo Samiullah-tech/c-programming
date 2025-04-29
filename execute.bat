447ef8c981e78e6fad99c9c27c7a82c5d5e2c6b8
Zeeshan Amjad <62217382+ZeeshanAmjad0495@users.noreply.github.com>
2025-04-29 17:52:12 +0500

    Update execute.bat

--- execute.bat
+++ execute.bat
@@ -18,7 +18,7 @@ REM Extract the base name of the file (without the extension)
 for %%F in ("%SOURCE_FILE%") do set EXEC_FILE=%%~nF
 
 REM Compile the C program
-gcc  "%SOURCE_FILE%" -o "%EXEC_FILE%.exe"
+gcc -Wall -Werror  "%SOURCE_FILE%" -o "%EXEC_FILE%.exe"
 
 REM Check if the compilation was successful
 if %errorlevel% neq 0 (
